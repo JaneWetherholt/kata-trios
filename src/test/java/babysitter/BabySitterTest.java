e18ebd47096df151fe0073d413cdf7b7eb831abc
Jane Wetherholt <janewetherholt@gmail.com>
2020-11-16 20:13:44 -0500

    Added rules of kata to both test file and calculator file

--- src/test/java/babysitter/BabySitterTest.java
+++ src/test/java/babysitter/BabySitterTest.java
@@ -13,3 +13,29 @@ public class BabySitterTest {
         assertThat(wage, is(12));
     }
 }
+
+
+// The Rules:
+// The babysitter
+
+// starts no earlier than 5:00PM.
+// leaves no later than 4:00AM.
+// gets paid $12/hour from the start of their shift to the children's bedtime.
+// gets paid $8/hour from bedtime to midnight.
+// gets paid $16/hour from midnight to the end of their shift.
+// will start their shift on the hour and end their shift on the hour.
+// The bedtime
+
+// will be between 8:00PM and 11:00PM.
+// can be before the start of the babysitter's shift.
+// can be after the end of the babysitter's shift.
+// The Feature:
+// As a babysitter,
+
+// In order to get paid for 1 night of work,
+
+// I want to calculate my nightly wage.
+
+// Create a method that takes the babysitter's start time, end time, and the children's 
+// bedtime and returns the amount that the babysitter is owed for that shift. 
+// You should use military/24 hour format for the start/end times.
